2d6ab271133c1a3c038362bd51479b2c37085678
sjadhwani <sumit.jadhwani@nice.com>
2023-06-17 19:35:42 +0530

    added anagram with tests

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,20 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
 
+  var count1 = new Array(26).fill(0);
+  var count2 = new Array(26).fill(0);
+
+  for(var i = 0; i < str1.length; i++) { 
+    count1[str1.toLowerCase().charCodeAt(i) - 'a'.charCodeAt()]++;
+    count2[str2.toLowerCase().charCodeAt(i) - 'a'.charCodeAt()]++;
+  }
+
+  for(var i = 0; i < count1.length; i++) {
+    if(count1[i] !== count2[i]) return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
